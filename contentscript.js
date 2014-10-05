5b29158cb62f96fe699df5192f597b30e3a19e9f
nathanyw <imanasianninja@gmail.com>
2014-10-04 23:16:50 -0700

    more commands

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,40 @@
 // pokemon showdown keyboard plugin
 
+keymap = {
+    96: "toggle_focus",
+
+    126: "forfeit",
+
+    116: "cancel",
+    121: "timer",
+    219: "enemy_hover",
+
+    113: "first_move",
+    119: "second_move",
+    101: "third_move",
+    114: "fourth_move",
+    109: "mega_evo",
+
+    97: "first_switch",
+    115: "second_switch",
+    100: "third_switch",
+    102: "fourth_switch",
+    103: "fifth_switch",
+    104: "sixth_switch"
+};
+
+keymap_text = {
+    96: "toggle_focus",
+};
+
 actions = {
+    "toggle_focus": function() {
+        if ($("textarea, input").is(":focus")) { $("form.chatbox textarea.textbox").blur(); }
+        else { $("form.chatbox textarea.textbox").html("").focus(); }
+    },
+
+    "forfeit": function() { $("div.tabbar a[href='" + window.location.pathname + "']").click(); },
+
     "cancel": function() { $("div.controls button[name='undoChoice']").click(); },
     "timer": function() { $("div.controls button[name='setTimer']").click(); },
 
@@ -8,6 +42,7 @@ actions = {
     "second_move": function() { $("div.movemenu button:nth-child(2)").click(); },
     "third_move": function() { $("div.movemenu button:nth-child(3)").click(); },
     "fourth_move": function() { $("div.movemenu button:nth-child(4)").click(); },
+    "mega_evo": function() { $("input[name='megaevo']").click(); },
 
     "first_switch": function() { $("div.switchmenu button:nth-child(1)").click(); },
     "second_switch": function() { $("div.switchmenu button:nth-child(2)").click(); },
@@ -23,39 +58,35 @@ actions = {
 };
 
 state = {
+    "text_focus": true,
     "enemy_hover": false
 };
 
 $(document).ready(function() {
-    bind();
+    console.log("plugin loaded");
     textListener();
 });
 
 function textListener() {
-    $(document).on('focus', 'textarea', function() { console.log("denis"); unbind(); } );
-    $(document).on('blur', 'textarea', function() { bind(); } );
+    $(document).on('focus', 'input, textarea', function() { console.log("focused"); unbind(); bind(keymap_text, false); } );
+    $(document).on('blur', 'input, textarea', function() { unbind(); bind(keymap, true); } );
 };
 
-function bind() {
-    $(document).on('keyup', 'body', function(e) {
-        if (e.keyCode == 84) actions["cancel"]();
-        else if (e.keyCode == 89) actions["timer"]();
-        else if (e.keyCode == 219) actions["enemy_hover"]();
-        
-        else if (e.keyCode == 81) actions["first_move"]();
-        else if (e.keyCode == 87) actions["second_move"]();
-        else if (e.keyCode == 69) actions["third_move"]();
-        else if (e.keyCode == 82) actions["fourth_move"]();
-        
-        else if (e.keyCode == 65) actions["first_switch"]();
-        else if (e.keyCode == 83) actions["second_switch"]();
-        else if (e.keyCode == 68) actions["third_switch"]();
-        else if (e.keyCode == 70) actions["fourth_switch"]();
-        else if (e.keyCode == 71) actions["fifth_switch"]();
-        else if (e.keyCode == 72) actions["sixth_switch"]();
+function urlListener() {
+    $(window).hashchange( function() {
+        if (window.location.path.substring(1, 7) == "battle") true;
+    });
+}
+
+function bind(kmap, ignore) {
+    $(document).on('keypress', 'body', function(e) {
+        if (ignore) e.preventDefault();
+        var cmd = kmap[e.keyCode];
+        console.log(cmd);
+        if (cmd) actions[cmd]();
     });
 };
 
 function unbind() {
-    $(document).off('keyup');
+    $(document).off('keypress', 'body');
 };
