872edb63bd79464e3a1f9daf010ce2a67e353b51
Andrew Chen <Andrew@airbears2-10-142-17-110.airbears2.1918.berkeley.edu>
2014-10-04 18:30:35 -0700

    Refactored into actions

--- contentscript.js
+++ contentscript.js
@@ -1,24 +1,25 @@
 // pokemon showdown keyboard plugin
 //
 
-$(document).ready(function() {
-    bind();
+actions = 
+{
+    "first_move": function(){$("div.movemenu button:nth-child(1)").click();},
+    "second_move": function(){$("div.movemenu button:nth-child(2)").click();},
+    "third_move": function(){$("div.movemenu button:nth-child(3)").click();},
+    "fourth_move": function(){$("div.movemenu button:nth-child(4)").click();}
 }
-
-function bind() {
-    console.log("extension loaded");
-    $(document).keyup(function(e) {
-        if (e.keyCode == 81) {
-            $("div.movemenu button:nth-child(1)").click();
-        }
-        if (e.keyCode == 87) {
-            $("div.movemenu button:nth-child(2)").click();
-        }
-        if (e.keyCode == 69) {
-            $("div.movemenu button:nth-child(3)").click();
-        }
-        if (e.keyCode == 82) {
-            $("div.movemenu button:nth-child(4)").click();
-        }
-    })
-}
+$("*").keyup(function(e) {
+    console.log(e);
+    if (e.keyCode == 81) {
+        actions["first_move"]()
+    }
+    if (e.keyCode == 87) {
+        actions["second_move"]()
+    }
+    if (e.keyCode == 69) {
+        actions["third_move"]()
+    }
+    if (e.keyCode == 82) {
+        actions["fourth_move"]()
+    }
+})
