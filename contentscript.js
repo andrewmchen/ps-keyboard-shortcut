64049930d9f006990e3ddd47aea342aa4b03bc1d
nathanyw <imanasianninja@gmail.com>
2014-10-04 18:10:50 -0700

    qwer for 4 moves

--- contentscript.js
+++ contentscript.js
@@ -1,1 +1,24 @@
 // pokemon showdown keyboard plugin
+//
+
+$(document).ready(function() {
+    bind();
+}
+
+function bind() {
+    console.log("extension loaded");
+    $(document).keyup(function(e) {
+        if (e.keyCode == 81) {
+            $("div.movemenu button:nth-child(1)").click();
+        }
+        if (e.keyCode == 87) {
+            $("div.movemenu button:nth-child(2)").click();
+        }
+        if (e.keyCode == 69) {
+            $("div.movemenu button:nth-child(3)").click();
+        }
+        if (e.keyCode == 82) {
+            $("div.movemenu button:nth-child(4)").click();
+        }
+    })
+}
